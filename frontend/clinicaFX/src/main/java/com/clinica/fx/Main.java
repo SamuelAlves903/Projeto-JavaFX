d1dde242b89cd9bc65534750891658446a19c696
Samuel <alvessamuel205@gmail.com>
2026-02-01 20:39:30 -0300

    Estrutura inicial frontend

---
+++ frontend/clinicaFX/src/main/java/com/clinica/fx/Main.java
@@ -0,0 +1,17 @@
+package com.clinica.fx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Clínica FX");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
